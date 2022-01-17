ca69c49429be9694a3015a65cc78de84c2932c39
Sungwook Kim <discerningacumen@gmail.com>
2022-01-17 16:55:30 +0900

    Initial Commit

---
+++ My_Bookself_Application/My_Bookself_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Bookself_ApplicationApp.swift
+//  My_Bookself_Application
+//
+//  Created by sungwook on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct My_Bookself_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
