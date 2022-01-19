191f55206908ba8597affea512796ec7f873004a
Sungwook Kim <discerningacumen@gmail.com>
2022-01-19 15:23:40 +0900

    I added Xcode automated DEBUG feature on TheControlPanel

--- My_Bookself_Application/Data Structures/TheControlPanel.swift
+++ My_Bookself_Application/Data Structures/TheControlPanel.swift
@@ -16,11 +16,15 @@ struct TheControlPanel {
     /// The purpose is to test it on the Simulator or the real iOS devices.
     /// When I need to test only the portion `BookView`, set it as the following example, saying, `TDD_Setting = TEST_BookView`
     enum TESTING_STATE { case TEST_BookView, TEST_ContentView, RELEASE }
+#if DEBUG
     /// In order to turn on the testing only BookView uncomment the following
-//    static let TDD_Setting = TESTING_STATE.TEST_BookView
+    //    static let TDD_Setting = TESTING_STATE.TEST_BookView
     /// When the above is uncommented, the following should be commented out.
     static let TDD_SETTING = TESTING_STATE.RELEASE
-    
+#else
+    /// When Xcode is in RELEASE mode, this sets **automatically** `TDD_SETTING = TESTING_STATE.RELEASE`.
+    static let TDD_SETTING = TESTING_STATE.RELEASE
+#endif
     
     
     // MARK: - Global Settings
